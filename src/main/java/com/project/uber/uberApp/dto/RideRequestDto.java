86f480998f32ffe1c968142488ab0437a965fff4
Thakur Bhanu Pratap Singh <bhanupratap983784@gmail.com>
2025-03-11 16:14:56 +0530

    Initial Setup Of Uber App

---
+++ src/main/java/com/project/uber/uberApp/dto/RideRequestDto.java
@@ -0,0 +1,24 @@
+package com.project.uber.uberApp.dto;
+
+import com.project.uber.uberApp.entities.enums.PaymentMethod;
+import com.project.uber.uberApp.entities.enums.RideRequestStatus;
+import org.locationtech.jts.geom.Point;
+
+import java.time.LocalDateTime;
+
+public class RideRequestDto {
+
+    private Long id;
+
+    private Point pickUpLocation;
+
+    private Point dropOffLocation;
+
+    private LocalDateTime requestedTime;
+
+    private RiderDto rider;
+
+    private PaymentMethod paymentMethod;
+
+    private RideRequestStatus rideRequestStatus;
+}
